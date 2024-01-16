d2ed090999495046ed487be4f1cd4ff9c2aa16ca
Brilliance Osprey(MyatSoe) <56672040+BrillianceOsprey@users.noreply.github.com>
2024-01-16 13:40:54 +0630

    vercel

---
+++ index.js
@@ -0,0 +1,36 @@
+// IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const adminRouter = require("./routes/admin");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+// INIT
+const PROT = 8080;
+const app = express();
+const DB =
+  "mongodb+srv://myatsoedev:myatsoedev2580@cluster0.pepznvb.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PROT, () => {
+  console.log(`connected at port ${PROT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose"); // Erase if already required
+const { productSchema } = require("./product");
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: (value) => {
+        const re =
+          /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return value.match(re);
+      },
+      message: "Please enter a valid email address",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => {
+        return value.length > 6;
+      },
+      message: "Please enter a long password",
+    },
+  },
+  address: {
+    type: String,
+    default: "",
+  },
+
+  type: {
+    type: String,
+    default: "user",
+  },
+  cart: [
+    {
+      product: productSchema,
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+//Export the model
+const User = mongoose.model("User", userSchema);
+module.exports = User;
