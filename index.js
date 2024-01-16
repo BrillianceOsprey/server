d7adb7469949eb099fdca829fd45e38fd5842f05
Brilliance Osprey(MyatSoe) <56672040+BrillianceOsprey@users.noreply.github.com>
2024-01-16 14:12:13 +0630

    vercel

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/admin");
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
+require("dotenv").config();
 
 // INIT
-const PROT = 8080;
+// const PROT = 8080;
 const app = express();
-const DB =
-  "mongodb+srv://myatsoedev:myatsoedev2580@cluster0.pepznvb.mongodb.net/?retryWrites=true&w=majority";
+// const DB =
+//   "mongodb+srv://myatsoedev:myatsoedev2580@cluster0.pepznvb.mongodb.net/?retryWrites=true&w=majority";
 
 // middleware
 app.use(express.json());
@@ -23,7 +24,7 @@ app.use(userRouter);
 
 // Connections
 mongoose
-  .connect(DB)
+  .connect(process.env.MONGODB_CONNECT_URL)
   .then(() => {
     console.log("Connection Successful");
   })
@@ -31,6 +32,6 @@ mongoose
     console.log(e);
   });
 
-app.listen(PROT, () => {
-  console.log(`connected at port ${PROT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`connected at port ${process.env.PORT}`);
 });
